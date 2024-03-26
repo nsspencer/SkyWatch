5b870738e8189792ec97e5c488474fdd0a37d893
Nathan Spencer <nathanspencer97@hotmail.com>
2024-03-26 00:42:12 -0400

    work in progress test framework

--- tests/look_angle_test.py
+++ tests/look_angle_test.py
@@ -1,3 +1,5 @@
+import unittest
+
 import astropy.units as u
 from astropy.time import Time
 
@@ -5,18 +7,20 @@ from skywatch.look_angles import LocalTangentENU
 from skywatch.platform import Platform
 from skywatch.skypath import SkyPath
 
-if __name__ == "__main__":
-    ground_station = Platform(
-        SkyPath.from_geodetic(Time("J2000"), 10 * u.deg, 10 * u.deg, 0 * u.m),
-        look_angles_strategy=LocalTangentENU(),
-        attitude=None,
-    )
 
-    ground_station2 = Platform(
-        SkyPath.from_geodetic(Time("J2000"), 19 * u.deg, 1 * u.deg, 155000 * u.m),
-        look_angles_strategy=LocalTangentENU(),
-        attitude=None,
-    )
+class Test1(unittest.TestCase):
+    def test_ground_station_look_angles(self):
+        ground_station = Platform(
+            SkyPath.from_geodetic(Time("J2000"), 10 * u.deg, 10 * u.deg, 0 * u.m),
+            look_angles_strategy=LocalTangentENU(),
+            attitude=None,
+        )
 
-    print(ground_station.look_angles(Time("J2000"), ground_station2))
-    print(ground_station2.look_angles(Time("J2000"), ground_station))
+        ground_station2 = Platform(
+            SkyPath.from_geodetic(Time("J2000"), 19 * u.deg, 1 * u.deg, 155000 * u.m),
+            look_angles_strategy=LocalTangentENU(),
+            attitude=None,
+        )
+
+        print(ground_station.look_angles(Time("J2000"), ground_station2))
+        print(ground_station2.look_angles(Time("J2000"), ground_station))
