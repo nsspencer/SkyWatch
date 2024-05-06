5f220fc2cb2c29c64c147b9bfb4852db0c859c4f
Nathan Spencer <nathanspencer97@hotmail.com>
2024-05-05 22:41:57 -0400

    added tqdm to requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="skywatch",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/nsspencer/SkyWatch",
     author="Nathan Spencer",
     description="Aerospace/astrodynamics analysis library providing high level interfaces for coordinate, attitude, access, and look angle calculations.",
     packages=find_packages(),
-    install_requires=["astropy", "numpy", "portion", "scipy", "pymap3d"],
+    install_requires=["astropy", "numpy", "portion", "scipy", "pymap3d", "tqdm"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
